150f989abc7feadc3c785d778980961306515695
sfxaabor <aabor@seamfix.com>
2026-01-29 16:44:52 +0100

    chore: setup ConfigModule and PostgreSQL database connection

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule, ConfigService } from "@nestjs/config"
+import { appConfig } from '../config/app.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const db = config.get('app.database');
+        return {
+          type: 'postgres',
+          host: db.host,
+          port: db.port,
+          username: db.username,
+          password: db.password,
+          database: db.name,
+          autoLoadEntities: db.autoLoadEntities,
+          synchronize: db.sync,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
