3e06742e0502655a23a188ac0df5b3213175904c
sfxaabor <aabor@seamfix.com>
2026-01-31 20:03:02 +0100

    feat: Created registration and login endpoints with validation

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,11 +1,28 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService){}
+  
   @UseGuards(JwtAuthGuard)
-  @Get('me')
+  @Get('/profile')
   getProfile(@Req() req) {
-    return req.user;
+    return this.authService.getProfile(req.user.sub)
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/signup')
+  async signup(@Body() createUserDto: RegisterDto) {
+    return await this.authService.signUp(createUserDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/login')
+  async login(@Body() loginDto: LoginDto) {
+    return await this.authService.login(loginDto);
   }
 }
--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { BcryptProvider } from './providers/bcrypt.provider';
+import { UserModule } from '../user/user.module';
 
 @Module({
     imports: [
@@ -19,10 +22,11 @@ import { AuthController } from './auth.controller';
                     signOptions: { expiresIn: env.jwtExpiresIn }
                 }
             }
-        })
+        }),
+        forwardRef(() => UserModule)
     ],
-    providers: [JwtStrategy],
-    exports: [JwtModule, PassportModule],
+    providers: [JwtStrategy, AuthService, BcryptProvider],
+    exports: [JwtModule, PassportModule, AuthService, BcryptProvider],
     controllers: [AuthController]
 })
 export class AuthModule {}
