d64dbeb18f3829a0025fb4fbc4fcaa119072402c
sfxjamoo <jamoo@seamfix.com>
2026-02-02 08:03:09 +0100

    (chore): tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./frontend/src/**/*.{html,ts}', './libs/**/src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
